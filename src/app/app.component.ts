25b7b47d90c3bdc57514bf5d5e97748d2f07abc5
cmulero83 <cmulero83@icloud.com>
2022-04-16 14:10:36 -0500

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+//
+// Trabajo con fromEvent
+// Esto lo que permite crear un evento como observable
+// Por ejemplo un keymove, etc, pasarlo a un observable
+//
+
+
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';  // Importamos fromEvent....
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs_2_fromEvent';
+
+
+
+  constructor(){}
+
+  public ngOnInit():void{
+    const el = document.getElementById('elemento')  // ya tenemos el elmento en el la contante, lo ideal seria usar viewchild
+    const mouseMove = fromEvent(el, 'mouseover')
+
+    mouseMove.subscribe( (e: any) => {
+      console.log(`Coordenadas: X : ${e.clientx}, Y: ${e.clienty}`)
+    })
+
+  }
+
 }
